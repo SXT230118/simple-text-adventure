cb4045a5a6f6f980a4386e6efc9137b04a5b0914
SXT230118 <sxt230118@utdallas.edu>
2026-01-03 23:53:30 -0600

    Added basic player save, creation, and loading functionality

--- story.py
+++ story.py
@@ -1,6 +1,11 @@
-# Player data
-player = {
-    "name": "Adventurer",
-    "coins": 100,
-    "inventory": []
-}
+# Player creation, with starting values
+def create_player():
+    # Player data
+    player = {
+        "coins": 0,
+        # Change fron list to dict bc easier to access with item name as key
+        "inventory": {},
+        "task_cooldown": 0,
+    }
+
+    return player
